1345a58bcd702a2b6af7bb9fb18e25791894b78e
Gabriel Moura <gabrielm.reis@outlook.com>
2026-03-19 23:43:04 -0300

    feat: criar metodos genericos em Clients

---
+++ src/ProductClientHub/ProductClientHub.API/Controllers/ClientsControllerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductClientHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsControllerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Created();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Edit([FromRoute] Guid id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            return NoContent();
+        }
+    }
+}
