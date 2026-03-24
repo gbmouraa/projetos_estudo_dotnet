7ee7f083839ed806a2080ab79c8b4404b07cdfe9
Gabriel Moura <gabrielm.reis@outlook.com>
2026-03-23 23:47:07 -0300

    feat: add ResponseAllClientsJson

---
+++ src/ProductClientHub/ProductClientHub.Communication/Responses/Clients/ResponseAllClientsJson.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductClientHub.Communication.Responses.Clients
+{
+    public class ResponseAllClientsJson
+    {
+        public List<ResponseShortClientJson> Clients { get; set; } = new List<ResponseShortClientJson>();
+    }
+}
